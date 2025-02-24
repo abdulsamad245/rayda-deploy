44cf7d419dca74c359608dd06babb2b1d4645506
Balogun Abdulsamad <abdulsamadbalogun25@gmail.com>
2025-02-24 10:46:33 +0100

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: "standalone", // Vercel deployment config
+  output: "standalone", // Vercel deployment config
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
